fd974bce2ce2053b0ce85e8fe4127be35ef8caf3
prasanthmp500 <prasanthmp500@gmail.com>
2013-08-19 15:29:33 +0530

    helloworld.java

---
+++ src/com/logica/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.logica.helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+ 
+		
+		System.out.println("First git hub project");
+
+	}
+
+}
